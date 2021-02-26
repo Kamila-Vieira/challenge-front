cdad20cd5489b0ccb855bedc4ce3ba63a4bc3dda
kamila.almeida <vkamila54@outlook.com>
2021-02-26 15:03:18 -0300

    Update cart code

--- js/mini-cart/index.js
+++ js/mini-cart/index.js
@@ -1,6 +1,11 @@
+const miniCart = document.querySelector(".mini-cart");
+const productsContainer = document.querySelector(".products-content");
+const totalPriceContainer = document.querySelector(
+  ".products-total-price span"
+);
 async function fetchProducts() {
-  let URL_CART = "../../products.json";
-  let response = await fetch(URL_CART);
+  const URL_CART = "./products.json";
+  const response = await fetch(URL_CART);
   let cartProducts = [];
   cartProducts = await response
     .json()
@@ -9,32 +14,31 @@ async function fetchProducts() {
       return cart.item;
     })
     .catch((err) => []);
-  console.log(cartProducts);
   createCartProducts(cartProducts);
 }
 fetchProducts();
-
 function createCartProducts(products) {
-  let productsPrice = [];
-  let productsContainer = document.querySelector(".products-content");
+  const productsPrice = [];
   if (products.length > 0) {
     products.forEach((product) => {
       const { productId, bestPriceFormated, name, image, quantity } = product;
-      productsPrice.push(bestPriceFormated);
-      let productContent = `<div class="cart-product" id="${productId}">
+      productsPrice.push({ quantity, bestPriceFormated });
+      const productContent = `<div class="cart-product" id="${productId}">
         <img src="${image}" alt="${name}" />
         <div class="product-info">
           <h3 class="product-name">${name}</h3>
           <div class="product-buy-info">
             <p class="product-quantity">Qtd.: ${quantity}</p>
-            <p class="product-price">${bestPriceFormated}</p>
+            <p class="product-price">${formatPrice(
+              calculateProductPrice(bestPriceFormated, quantity)
+            )}</p>
           </div>
         </div>
       </div>`;
       productsContainer.insertAdjacentHTML("afterbegin", productContent);
     });
   } else {
-    let voidCart = `<div class="void-cart"">
+    const voidCart = `<div class="void-cart"">
     <h3>Carrinho vazio!</h3>
     </div>`;
     productsContainer.innerHTML = voidCart;
@@ -42,26 +46,29 @@ function createCartProducts(products) {
   setTotalProductsPrice(productsPrice);
 }
 function setTotalProductsPrice(prices) {
-  const totalPriceContainer = document.querySelector(
-    ".products-total-price span"
-  );
-  let totalPrice = prices
-    .map((price) =>
-      parseFloat(price.replace(/[(R$ ).]/g, "").replace(",", "."))
-    )
-    .reduce((acc, cur) => acc + cur, 0)
-    .toLocaleString("pt-br", {
-      style: "currency",
-      currency: "BRL",
-      minimumFractionDigits: 2,
-    });
-  totalPriceContainer.innerHTML = totalPrice;
+  const totalPrice = prices
+    .map((price) => {
+      const { quantity, bestPriceFormated } = price;
+      return calculateProductPrice(bestPriceFormated, quantity);
+    })
+    .reduce((acc, cur) => acc + cur, 0);
+  totalPriceContainer.innerHTML = formatPrice(totalPrice);
 }
-
-const miniCart = document.querySelector(".mini-cart");
-
-miniCart.addEventListener("click", function () {
+function calculateProductPrice(price, quantity) {
+  return (
+    quantity * parseFloat(price.replace(/[(R$ ).]/g, "").replace(",", "."))
+  );
+}
+function formatPrice(price) {
+  return price.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 2,
+  });
+}
+function toggleMiniCart() {
   document
     .querySelector(".products-container")
     .classList.toggle("show-mini-cart");
-});
+}
+miniCart.addEventListener("click", toggleMiniCart);
